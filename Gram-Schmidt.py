9bc0b28cfb36f3922246bf86245de21e89ea53e7
Manauwar Zulfekar Ansari <mza.ansari003@live.com>
2020-04-16 18:11:37 +0530

    Create Gram-Schmidt.py
    Gram-Schmidth Process is an Algorithm taught in any standard linear algebra course,the aim is to covert any set of vectors into a orthogonal set of vectors,which has application in solving system of linear equations.

---
+++ Gram-Schmidt.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+#This Function defines an inner product
+def inner_product(u,v):
+    return np.dot(u,v)
+    
+#This function defines projection of v onto u
+def proj(u,v): 
+    return (inner_product(v,u) / inner_product(u,u))*u
+
+def gs_process(vectors):
+    n = len(vectors)     # number of vectors in a list
+    vectors = np.array(vectors) # convert vectors into a numpy array
+    m = max(vectors.shape)  # number of entries in each vector
+    u = np.zeros((n,m))     # creates an array of shape n by m
+    
+    u[0] = vectors[0]
+
+    for i in range(1,n):
+        sum_vector = np.zeros(m)
+        for j in range(0,i):
+            sum_vector = sum_vector + proj(u[j],vectors[i])
+        u[i] = vectors[i] - sum_vector
+    return u
+
+vectors = [[1,-1,1,-1],[1,1,3,-1],[-3,7,1,3]]
+print(gs_process(vectors))
+
+# After running the code we get
+#[[ 1. -1.  1. -1.]
+#[ 0.  2.  2.  0.]
+#[ 0.  0.  0.  0.]]
+
+For more information about go this [link]:(https://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process)
